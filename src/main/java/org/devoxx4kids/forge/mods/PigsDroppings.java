bc4e27d14e9165f530843ac81f4b7ebc7bfebf47
Victoria <victoria.dvornikova@gmail.com>
2015-09-20 21:32:22 -0400

    a mod that makes pigs drop diamonds and potatoes in large numbers:), and makes an explosion when you pick up a diamond:(

--- src/main/java/org/devoxx4kids/forge/mods/PigsDroppings.java
+++ src/main/java/org/devoxx4kids/forge/mods/PigsDroppings.java
@@ -1,10 +1,13 @@
 package org.devoxx4kids.forge.mods;
 
 import scala.util.Random;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.passive.EntityPig;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class PigsDroppings {
@@ -17,8 +20,18 @@ public class PigsDroppings {
 		Random random = new Random();
 		
 		if(!event.entity.worldObj.isRemote){
-			event.entity.dropItem(Item.getItemFromBlock(Blocks.wool), random.nextInt(10));
+			event.entity.dropItem(Items.diamond, random.nextInt(10));
+			event.entity.dropItem(Items.potato, random.nextInt(6));
 		}
+	}
+	@SubscribeEvent
+	public void explodingDiamonds(EntityItemPickupEvent event){
+		if(event.item.getEntityItem().getItem() !=
+				Items.diamond) {
+			return;
+		}
+		EntityItem diamond = event.item;
+		event.entity.worldObj.createExplosion(diamond, diamond.posX, diamond.posY, diamond.posZ, 3, true);
 	}
 
 }
